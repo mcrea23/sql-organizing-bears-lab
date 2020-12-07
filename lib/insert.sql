048a781d7f340421991b9fb8b3231e7c5f4e8c22
+ <mcrea23@aol.com>
2020-12-07 13:11:46 -0600

    commit tree ce51b5152fcc1a1e34488177fdf65c2777999ecd

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 23, "M", "hazelnut brown", "chill", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "blue", "wild", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 40, "F", "light brown", "caring", 1);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 55, "M", "midnight black", "intense", 0);
+INSERT INTO bears VALUES (5, "Melissa", 28, "F", "hazelnut brown", "cutie", 1);
+INSERT INTO bears VALUES (6, "Grinch", 80, "M", "green", "mean", 0);
+INSERT INTO bears VALUES (7, "Wendy", 8, "F", "pink", "playful", 1);
+INSERT INTO bears VALUES (8, null, 15, "M", "seethrough", "shy", 0);
